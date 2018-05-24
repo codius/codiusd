8623073f7c09b4618ef4005edaf0084e54b32207
sharafian <ben@sharafian.me>
2018-05-24 11:32:43 -0700

    feat: switch to spawn to create pods

--- src/services/PodManager.ts
+++ src/services/PodManager.ts
@@ -1,10 +1,33 @@
-import axios from 'axios'
+// import axios from 'axios'
+import { spawn } from 'child_process'
+import tempy from 'tempy'
+import fs from 'fs-extra'
 
 export default class PodManager {
   async startPod (podSpec: object) {
-    await axios('/pod/start', {
-      method: 'post',
-      socketPath: '/var/run/hyper.sock'
+    // TODO: switch this to an HTTP request
+    // await axios('/pod/start', {
+    //   method: 'post',
+    //   socketPath: '/var/run/hyper.sock'
+    // })
+
+    const tmpFile = tempy.file({ extension: 'json' })
+    await fs.writeJson(tmpFile, podSpec)
+
+    const start = spawn('hyperctl', [
+      'run', '--rm', '-p', tmpFile
+    ])
+
+    start.stdout.pipe(process.stderr)
+    start.stderr.pipe(process.stderr)
+
+    return new Promise((resolve, reject) => {
+      start.on('close', code => {
+        if (code) reject(new Error(`command failed. ` +
+          `code=${code} `
+          `command="hyperctl run --rm -p ${tmpFile}"`))
+        resolve()
+      })
     })
   }
 }
