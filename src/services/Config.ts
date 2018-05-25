a98355cc0d2e5fc4dd655901ca772661ed728fe4
sharafian <ben@sharafian.me>
2018-05-24 17:16:53 -0700

    feat: delete pods at expiry

--- src/services/Config.ts
+++ src/services/Config.ts
@@ -1,6 +1,7 @@
 import { Injector } from 'reduct'
 
 export default class Config {
+  readonly noop: boolean
   readonly port: number
   readonly publicUri: string
   readonly bootstrapPeers: string
@@ -11,6 +12,7 @@ export default class Config {
       env = process.env
     }
 
+    this.noop = env.CODIUS_HYPER_NOOP === 'true'
     this.port = Number(env.CODIUS_PORT) || 3000
     this.publicUri = env.CODIUS_PUBLIC_URI || ('http://localhost:' + this.port)
     this.bootstrapPeers = env.CODIUS_BOOTSTRAP_PEERS
