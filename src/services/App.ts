06a1c825b5c80f3917f6b7b4d651aaf3fac2736a
sharafian <ben@sharafian.me>
2018-06-03 18:02:13 -0700

    feat: persist money

--- src/services/App.ts
+++ src/services/App.ts
@@ -4,6 +4,7 @@ import Config from './Config'
 import PodManager from './PodManager'
 import PeerFinder from './PeerFinder'
 import HttpServer from './HttpServer'
+import Secret from './Secret'
 import Money from './Money'
 
 import { create as createLogger } from '../common/log'
@@ -14,6 +15,7 @@ export default class App {
   private peerFinder: PeerFinder
   private httpServer: HttpServer
   private podManager: PodManager
+  private secret: Secret
   private money: Money
 
   constructor (deps: Injector) {
@@ -24,11 +26,13 @@ export default class App {
     this.peerFinder = deps(PeerFinder)
     this.httpServer = deps(HttpServer)
     this.podManager = deps(PodManager)
+    this.secret = deps(Secret)
     this.money = deps(Money)
   }
 
   async start () {
     log.info('starting codiusd...')
+    await this.secret.load()
     await this.money.start()
     await this.httpServer.start()
     this.peerFinder.start()
@@ -37,7 +41,7 @@ export default class App {
 
   private makeRootDir () {
     try {
-      mkdir(this.config.codiusRoot, 0o755)
+      mkdir(this.config.codiusRoot, 0o700)
     } catch (err) {
       if (err.code !== 'EEXIST') {
         throw err
