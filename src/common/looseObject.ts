008ecd66288526cf2966adaa218333f9afe79cfb
Andros Wong <androswong418@gmail.com>
2018-06-12 17:11:08 -0700

    Last few fixes

--- src/common/looseObject.ts
+++
@@ -1,5 +0,0 @@
-interface LooseObject {
-  [key: string]: any
-}
-
-export default LooseObject
--- src/schemas/HostInfo.ts
+++ src/schemas/HostInfo.ts
@@ -11,5 +11,5 @@ export interface HostInfo {
   costPerMonth: number;
   uri: string;
   runningContracts?: number;
-  peers?: any[];
+  peers?: string[];
 }
