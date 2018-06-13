da4544486d56f2e88779dbac49afce3ca46da7b8
Andros Wong <androswong@Andross-MacBook-Pro.local>
2018-06-13 15:00:59 -0700

    Fix validatePeer.ts

--- src/util/validatePeer.ts
+++ src/util/validatePeer.ts
@@ -1,22 +1,19 @@
 const url = require('url')
 const IGNORE_LIST = ['localhost', '0.0.0.0', 'local.codius.org']
-import * as reduct from 'reduct'
-import Config from '../services/Config'
-const config = reduct()(Config)
 
 export function shallowValidatePeer (peer?: string): boolean {
   if (!peer) {
     return false
   }
 
-  if (!config.devMode && !peer.includes('https')) {
-    return false
+  if (process.env.CODIUS_DEV === 'true') {
+    return true
   }
 
   let hostName = url.parse(peer)
-  if (!hostName) {
+  if (!hostName.hostname) {
     return false
-  } else if (hostName.hostname.slice(0,3) === '127' || IGNORE_LIST.includes(hostName.hostname)) {
+  } else if (hostName.hostname.slice(0,3) === '127' || IGNORE_LIST.includes(hostName.hostname) || hostName.protocol !== 'https:') {
     return false
   }
   return true
