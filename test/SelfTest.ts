8f1bd7b700c4a018de0dd289f1ee61c0950ace5d
Andros Wong <androswong418@gmail.com>
2018-07-16 09:35:15 -0700

    Exit service if self test fails and disableSelfTest not set to true in env

--- test/SelfTest.ts
+++ test/SelfTest.ts
@@ -47,21 +47,33 @@ export default class SelfTest {
           })
 
           ws.on('error', (err: any) => {
-            log.debug('Error on connection', err)
+            log.debug('Error on connection for websockets', err)
+            if (!this.config.disableSelfTest) {
+              process.exit(1)
+            }
           })
         })
 
         const serverPromise = new Promise(async resolve => {
-          const serverRes = await axios.get(`https://${response.manifestHash}.${url.host}/server`)
-          const serverCheck = serverRes.data
-          log.debug('serverProm', serverCheck)
-          if (serverCheck.imageUploaded) {
-            // resolve promise
-            resolve()
+          try {
+            const serverRes = await axios.get(`https://${response.manifestHash}.${url.host}/server`)
+            const serverCheck = serverRes.data
+            log.debug('serverProm', serverCheck)
+            if (serverCheck.imageUploaded) {
+              // resolve promise
+              resolve()
+            }
+          } catch (err) {
+            log.error('Test contract not running')
+            if (!this.config.disableSelfTest) {
+              process.exit(1)
+            }
           }
         })
         const testPromises = await Promise.all([serverPromise, webSocketPromise])
         try {
+          // Test that none of these promises are hanging for more than 60 seconds
+          // Timeout is set so that the contract has time to be pulled.
           setTimeout(() => {
             Promise.race([
               testPromises,
@@ -72,16 +84,23 @@ export default class SelfTest {
                 }, 60000)
               })
             ])
-          }, 10000)
+          }, 20000)
         } catch (e) {
           log.debug('Server or web socket Error', e)
+          if (!this.config.disableSelfTest) {
+            process.exit(1)
+          }
         }
 
       } else {
-        throw new Error(`Failed to upload contract due to: ${response.error}`)
+        log.error(`Failed to upload contract due to: ${response.error}`)
+        throw new Error(`Could not upload contract successfully due to: ${response.error}`)
       }
     } catch (e) {
       log.debug('Upload Error', e)
+      if (!this.config.disableSelfTest) {
+        process.exit(1)
+      }
     }
   }
 
