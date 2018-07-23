9a0648e24447635daa9fb43dd3078eddcedb06ae
Nathan Lie <lie4nathan@gmail.com>
2018-07-23 15:06:03 -0700

    try/catch

--- src/services/PodManager.ts
+++ src/services/PodManager.ts
@@ -119,16 +119,19 @@ export default class PodManager {
       await this.pods.setPodPort(podSpec.id, port)
     }
     console.log('get hyper to run pod...')
-    await this.hyperClient.runPod(podSpec).catch(async (e) => {
-      console.log(e)
-      console.log('waiting 5s to throw')
-      await new Promise(resolve => {
-        setTimeout(() => {
-          console.log('waited to throw')
-          resolve()
+    try {
+      await this.hyperClient.runPod(podSpec)
+    } catch (e) {
+        console.log(e)
+        console.log('waiting 5s to throw')
+        await new Promise(resolve => {
+          setTimeout(() => {
+            console.log('waited to throw')
+            resolve()
+          })
         })
-      })
-      throw Boom.conflict('this conflicted')
+        throw Boom.conflict('this conflicted')
+      }
     })
     console.log('hyper ran pod.')
 
