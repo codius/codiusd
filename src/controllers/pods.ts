fc125e6d4a2d387c295c0fff585f0b90cb053fae
Travis Crist <4206970+traviscrist@users.noreply.github.com>
2018-07-23 17:40:38 -0700

    improve payment required error 2

--- src/controllers/pods.ts
+++ src/controllers/pods.ts
@@ -64,9 +64,9 @@ export default function (server: Hapi.Server, deps: Injector) {
     const currencyPerSecond = getCurrencyPerSecond(config)
     const price = currencyPerSecond.times(new BigNumber(duration)).integerValue(BigNumber.ROUND_CEIL)
     log.debug('got post pod request. duration=' + duration + ' price=' + price.toString())
-    let stream
+
+    const stream = request.ilpStream()
     try {
-      stream = request.ilpStream()
       await stream.receiveTotal(price)
     } catch (e) {
       // TODO: use logger module
