f9e363ae25570d1e75e930147a39474684e3daa3
SneakyFish5 <32284796+SneakyFish5@users.noreply.github.com>
2018-08-05 21:31:26 -0500

    Make uptime stats more human readable in index page

--- src/controllers/static.ts
+++ src/controllers/static.ts
@@ -14,6 +14,35 @@ export default function (server: Hapi.Server, deps: Injector) {
   const config = deps(Config)
   const ver = deps(Version)
 
+  function uptimeToStr (uptime: any) {
+
+    function numberEnding (time: any) {
+      return (time > 1 || time === 0) ? `s` : ``
+    }
+
+    const years = Math.floor(uptime / 31536000)
+    const months = Math.floor((uptime %= 31536000) / 2592000)
+    const days = Math.floor((uptime %= 2592000) / 86400)
+    const hours = Math.floor((uptime %= 86400) / 3600)
+    const minutes = Math.floor((uptime %= 3600) / 60)
+    const seconds = Math.floor(uptime % 60)
+
+    if (years) {
+      return `${years} year${numberEnding(years)} ${months} month${numberEnding(months)} ${days} day${numberEnding(days)}`
+    } else if (months) {
+      return `${months} month${numberEnding(months)} ${days} day${numberEnding(days)}`
+    } else if (days) {
+      return `${days} day${numberEnding(days)} ${hours} hour${numberEnding(hours)}`
+    } else if (hours) {
+      return `${hours} hour${numberEnding(hours)} ${minutes} minute${numberEnding(minutes)}`
+    } else if (minutes) {
+      return `${minutes} minute${numberEnding(minutes)}`
+    } else if (seconds) {
+      return `${seconds} second${numberEnding(seconds)}`
+    }
+    return 'less than a second'
+  }
+
   async function getIndex (request: Hapi.Request, h: Hapi.ResponseToolkit) {
     const hostInfo: HostInfo = serverInfo(config, podManager, peerDb)
     const freeMemRaw = hostInfo.serverFreeMemory
@@ -21,8 +50,8 @@ export default function (server: Hapi.Server, deps: Injector) {
     return h.view('index', {
       uri: hostInfo.uri,
       numPeers: hostInfo.numPeers,
-      serverUptime: hostInfo.serverUptime,
-      serviceUptime: hostInfo.serviceUptime,
+      serverUptime: uptimeToStr(hostInfo.serverUptime),
+      serviceUptime: uptimeToStr(hostInfo.serviceUptime),
       avgLoad: hostInfo.avgLoad,
       currency: hostInfo.currency,
       costPerMonth: hostInfo.costPerMonth,
